7ac1f368fb0d3c74a71553b9d42f3ea1bbff8df9
Brian Fraser <bfraser@frasertechnicalsolutions.com>
2019-02-10 12:44:46 -0500

    add build script

---
+++ scripts/build.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = 'production';
+process.env.NODE_ENV = 'production';
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+// Ensure environment variables are read.
+require('../config/env');
+
+const path = require('path');
+const paths = require('../config/paths');
+const fs = require('fs-extra');
+const webpack = require('webpack');
+const config = require('../config/webpack.config');
+
+fs.emptyDirSync(paths.appBuild);
+copyPublicFolder();
+let compiler = webpack(config);
+compiler.run((err, stats) => {
+  console.log('done');
+});
+
+function copyPublicFolder() {
+  fs.copySync(paths.appPublic, paths.appBuild, {
+    dereference: true,
+    filter: file => file !== paths.appHtml
+  });
+}
