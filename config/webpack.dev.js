709cff8782d9c8961b0ff3b5206027bef28a534b
Brian Fraser <brian.fraser@ascorp.assuresign.com>
2019-02-16 10:46:33 -0500

    add url-loader

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -23,79 +23,90 @@ module.exports = merge(common, {
         filename: 'static/js/bundle.js',
         chunkFilename: 'static/js/[name].chunk.js',
         publicPath: '/',
-      //  devtoolModuleFilenameTemplate
+        //  devtoolModuleFilenameTemplate
     },
     optimization: {
         minimize: false,
         minimizer: [
-          // This is only used in production mode
-          new TerserPlugin({
-            terserOptions: {
-              parse: {
-                // we want terser to parse ecma 8 code. However, we don't want it
-                // to apply any minfication steps that turns valid ecma 5 code
-                // into invalid ecma 5 code. This is why the 'compress' and 'output'
-                // sections only apply transformations that are ecma 5 safe
-                // https://github.com/facebook/create-react-app/pull/4234
-                ecma: 8,
-              },
-              compress: {
-                ecma: 5,
-                warnings: false,
-                // Disabled because of an issue with Uglify breaking seemingly valid code:
-                // https://github.com/facebook/create-react-app/issues/2376
-                // Pending further investigation:
-                // https://github.com/mishoo/UglifyJS2/issues/2011
-                comparisons: false,
-                // Disabled because of an issue with Terser breaking valid code:
-                // https://github.com/facebook/create-react-app/issues/5250
-                // Pending futher investigation:
-                // https://github.com/terser-js/terser/issues/120
-                inline: 2,
-              },
-              mangle: {
-                safari10: true,
-              },
-              output: {
-                ecma: 5,
-                comments: false,
-                // Turned on because emoji and regex is not minified properly using default
-                // https://github.com/facebook/create-react-app/issues/2488
-                ascii_only: true,
-              },
-            },
-            // Use multi-process parallel running to improve the build speed
-            // Default number of concurrent runs: os.cpus().length - 1
-            parallel: true,
-            // Enable file caching
-            cache: true,
-            sourceMap: false,
-          }),
-          // This is only used in production mode
-          new OptimizeCSSAssetsPlugin({
-            cssProcessorOptions: {
-              parser: safePostCssParser,
-              map: false
-            },
-          }),
+            // This is only used in production mode
+            new TerserPlugin({
+                terserOptions: {
+                    parse: {
+                        // we want terser to parse ecma 8 code. However, we don't want it
+                        // to apply any minfication steps that turns valid ecma 5 code
+                        // into invalid ecma 5 code. This is why the 'compress' and 'output'
+                        // sections only apply transformations that are ecma 5 safe
+                        // https://github.com/facebook/create-react-app/pull/4234
+                        ecma: 8,
+                    },
+                    compress: {
+                        ecma: 5,
+                        warnings: false,
+                        // Disabled because of an issue with Uglify breaking seemingly valid code:
+                        // https://github.com/facebook/create-react-app/issues/2376
+                        // Pending further investigation:
+                        // https://github.com/mishoo/UglifyJS2/issues/2011
+                        comparisons: false,
+                        // Disabled because of an issue with Terser breaking valid code:
+                        // https://github.com/facebook/create-react-app/issues/5250
+                        // Pending futher investigation:
+                        // https://github.com/terser-js/terser/issues/120
+                        inline: 2,
+                    },
+                    mangle: {
+                        safari10: true,
+                    },
+                    output: {
+                        ecma: 5,
+                        comments: false,
+                        // Turned on because emoji and regex is not minified properly using default
+                        // https://github.com/facebook/create-react-app/issues/2488
+                        ascii_only: true,
+                    },
+                },
+                // Use multi-process parallel running to improve the build speed
+                // Default number of concurrent runs: os.cpus().length - 1
+                parallel: true,
+                // Enable file caching
+                cache: true,
+                sourceMap: false,
+            }),
+            // This is only used in production mode
+            new OptimizeCSSAssetsPlugin({
+                cssProcessorOptions: {
+                    parser: safePostCssParser,
+                    map: false
+                },
+            }),
         ],
         // Automatically split vendor and commons
         // https://twitter.com/wSokra/status/969633336732905474
         // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
         splitChunks: {
-          chunks: 'all',
-          name: true,
+            chunks: 'all',
+            name: true,
         },
         // Keep the runtime chunk separated to enable long term caching
         // https://twitter.com/wSokra/status/969679223278505985
         runtimeChunk: true,
-      },
+    },
     devServer: {
         contentBase: './build'
     },
     module: {
         rules: [{
             oneOf: [
+                // "url" loader works like "file" loader except that it embeds assets
+                // smaller than specified limit in bytes as data URLs to avoid requests.
+                // A missing `test` is equivalent to a match.
+                {
+                    test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                    loader: require.resolve('url-loader'),
+                    options: {
+                        limit: 10000,
+                        name: 'static/media/[name].[hash:8].[ext]',
+                    },
+                },
                 // Process application JS with Babel.
                 // The preset includes JSX, Flow, TypeScript, and some ESnext features.
                 {
